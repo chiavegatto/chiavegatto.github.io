f69b04b0d2313deddf71514c3bd39f53852f4a28
Rafael Chiavegatto <rafael.chiavegatto@fpf.br>
2019-08-14 12:23:35 -0400

    Initial commit

---
+++ JavaScript1.js
@@ -0,0 +1,27 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
+
+function Limpar() {
+  document.getElementById('meu_form').reset();
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
